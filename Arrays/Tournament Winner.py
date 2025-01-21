c43efcce2ad9b0b2ae9938b5008f2bb969767f3f
Hsin Kai <hsinkai2000@gmail.com>
2025-01-22 00:35:41 +0800

    Tournament Winner

---
+++ Arrays/Tournament Winner.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+
+
+def tournamentWinner(competitions, results):
+    # Write your code here.
+
+    map = defaultdict(int)
+
+    for i in range(len(competitions)):
+        if results[i] == 1:
+            winIndex = 0
+        else:
+            winIndex = 1
+
+        print(competitions[i][winIndex])
+        map[competitions[i][winIndex]] += 3
+
+        print(map)
+
+    return max(map, key=map.get)
